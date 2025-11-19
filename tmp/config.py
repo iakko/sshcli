8d519e5af355cf09477e9a65a53add257a8ee722
Iacopo Palazzi <iacopo.palazzi@gmail.com>
2025-11-19 12:41:38 +0100

    Improving test code coverage

--- tmp/config.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-"""Compatibility shim: importers of `sshcli.config` get the core config module."""
-
-import sys as _sys
-
-from sshcore import config as _core_config
-
-_sys.modules[__name__] = _core_config
